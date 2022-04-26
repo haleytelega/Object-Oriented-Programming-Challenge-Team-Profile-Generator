920f1ed54bd902053a0eb9de1a014b53f35f228b
Haley Telega <haleytelega@outlook.com>
2022-04-25 20:19:15 -0400

    added questions

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
 const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
+
+const questions = () => {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the managers name?'
+    },
+    {
+        type: 'input',
+        name: 'managerID',
+        message: 'Managers ID Number:'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'Managers Email:'
+    },
+    {
+        type: 'input',
+        name: 'managerOffice',
+        message: 'Managers Office Number:'
+    },
+    {
+        type: 'list',
+        name: 'employeeType',
+        message: 'Employee Type:',
+        choices: ["Engineer", "Intern", "Finish"]
+    },
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'Engineer Name:'
+    },
+    {
+        type: 'input',
+        name: 'engineerID',
+        message: 'Engineer ID Number:'
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'Engineer Email:'
+    },
+    {
+        type: 'input',
+        name: 'engineerGitHub',
+        message: 'Engineer GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'Intern Name:'
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        message: 'Intern ID Number:'
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: 'Intern Email:'
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Intern School'
+    }
+]);
+};
+
+questions();
