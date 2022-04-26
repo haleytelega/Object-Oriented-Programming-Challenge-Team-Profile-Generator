efc4b6e679a09f24c47d91c889ade0b02909fc3a
Haley Telega <haleytelega@outlook.com>
2022-04-25 20:24:06 -0400

    seperated questions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
 
-const questions = () => {
+const managerQuestions = () => {
     inquirer
     .prompt([
     {
@@ -30,48 +30,54 @@ const questions = () => {
         name: 'employeeType',
         message: 'Employee Type:',
         choices: ["Engineer", "Intern", "Finish"]
-    },
-    {
-        type: 'input',
-        name: 'engineerName',
-        message: 'Engineer Name:'
-    },
-    {
-        type: 'input',
-        name: 'engineerID',
-        message: 'Engineer ID Number:'
-    },
-    {
-        type: 'input',
-        name: 'engineerEmail',
-        message: 'Engineer Email:'
-    },
-    {
-        type: 'input',
-        name: 'engineerGitHub',
-        message: 'Engineer GitHub username:'
-    },
-    {
-        type: 'input',
-        name: 'internName',
-        message: 'Intern Name:'
-    },
-    {
-        type: 'input',
-        name: 'internID',
-        message: 'Intern ID Number:'
-    },
-    {
-        type: 'input',
-        name: 'internEmail',
-        message: 'Intern Email:'
-    },
-    {
-        type: 'input',
-        name: 'internSchool',
-        message: 'Intern School'
     }
 ]);
 };
 
-questions();
+const employeeQuestions = () =>{
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'Engineer Name:'
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: 'Engineer ID Number:'
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'Engineer Email:'
+        },
+        {
+            type: 'input',
+            name: 'engineerGitHub',
+            message: 'Engineer GitHub username:'
+        },
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'Intern Name:'
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: 'Intern ID Number:'
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'Intern Email:'
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'Intern School'
+        }
+    ]);
+};
+
+employeeQuestions();
